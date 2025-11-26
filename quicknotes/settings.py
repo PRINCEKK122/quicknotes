ddb9e83079c42117cb36424eee06ee477102c41b
Prince Karikari <awuahkarikari12@gmail.com>
2025-11-26 02:59:03 -0600

    add localhost and whitenoise

--- quicknotes/settings.py
+++ quicknotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@falb5v+sl%9i6+-r2dy*zsd&u(i25pm=2vcr4hapdh!y85r@@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["quicknotes-lp9k.onrender.com"]
+ALLOWED_HOSTS = ["quicknotes-lp9k.onrender.com", "127.0.0.1"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "quicknotes.urls"
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
